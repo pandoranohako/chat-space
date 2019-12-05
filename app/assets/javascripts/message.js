84b161216e4b809a192e3b5288406ca5adda32c6
pandoranohako <m.o_5963@icloud.com>
2019-12-05 16:26:08 +0900

    indent edit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -10,10 +10,12 @@ $(function(){
       <div class="message__upper-info__talker"> 
         ${message.user_name}
       </div>
+
       <div class="message__upper-info__date">
       ${message.created_at}
       </div>
         </div>
+
       <div class="message__text">
       <p class="message__text__content">
       ${content}
@@ -36,6 +38,7 @@ $('.new_message').on('submit', function(e){
     contentType: false
    })
     .done(function(data){
+      console.table(data)
       var html = buildHTML(data);
       $('.messages').append(html);	
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
